85a3c15de50dec148689e1a1437cbb9886d2ee36
Valerie Wade <valwade275@gmail.com>
2024-01-16 07:59:12 -0600

    output and variable exercises

---
+++ jsgreenbook/chapter-1-start/output.js
@@ -0,0 +1,4 @@
+/* This exercise is meant to display output in three ways. */  
+document.getElementById( 'message' ).innerText = 'Hello from Valerie!'
+window.alert( 'Hello from Valerie!' )
+console.log( 'Hello from Valerie!' )
---
+++ jsgreenbook/chapter-1-start/variables.js
@@ -0,0 +1,19 @@
+// The purpose of this exercise is to declare & initialize variables.
+// This exercise also explores the 'typeof' keyword.
+const firstName = 'Valerie' 
+const valueOfPi = 3.142
+let isValid = true
+let jsObject = console 
+let jsMethod = console.log
+let jsSymbol = Symbol( )
+let emptyVariable = null 
+let unusedVariable 
+
+console.log( 'firstName: ' + typeof firstName )
+console.log( 'valueOfPi: ' + typeof valueOfPi )
+console.log( 'isValid: ' + typeof isValid )
+console.log( 'jsObject: ' + typeof jsObject )
+console.log( 'jsMethod: ' + typeof jsMethod )
+console.log( 'jsSymbol: ' + typeof jsSymbol )
+console.log( 'emptyVariable: ' + typeof emptyVariable ) 
+console.log( 'unusedVariable: ' + typeof unusedVariable )
